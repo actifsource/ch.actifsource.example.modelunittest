5c1c8baecf9871f245074614bd952592dc1429e0
ruti <grischa.rutishauser@actifsource.com>
2020-05-19 07:45:02 +0200

    Add Unit test model

--- src/ch/actifsource/example/modelunittest/model/ChildTest.java
+++ src/ch/actifsource/example/modelunittest/model/ChildTest.java
@@ -2,7 +2,10 @@ package ch.actifsource.example.modelunittest.model;
 
 import java.io.IOException;
 import org.junit.Test;
+import ch.actifsource.core.PackagedResource;
 import ch.actifsource.core.job.Select;
+import ch.actifsource.core.job.Update;
+import ch.actifsource.core.patch.IStatementPosition;
 import ch.actifsource.core.selector.typesystem.impl.TypeSystem;
 import ch.actifsource.example.modelunittest.generic.GenericPackage;
 import ch.actifsource.example.modelunittest.generic.TestFunctionSpace.IRootFunctions;
@@ -43,5 +46,14 @@ public class ChildTest extends SingleScopeTestCase {
 		IRoot root = TypeSystem.getCompatibleDynamicResourceRepository(fSession).getResource(IRoot.class, SpecificPackage.RootInstance);
 		assertEquals(5, root.extension(IRootFunctions.class).getChildsJavaFunction().size());
 		assertEquals(5, RootFunctionsImpl.INSTANCE.getChildsJavaFunction(root).size());	
+	}
+	
+	@Test
+	public void test_createChild() {
+		assertEquals(5, Select.objectsForRelation(fSession, GenericPackage.Root_child, SpecificPackage.RootInstance).size());
+		
+		PackagedResource rootInstancePackageResource = Select.asPackagedResource(fSession, SpecificPackage.RootInstance);
+		Update.createAndInitializeResource(fWriteJobExecutor, rootInstancePackageResource.getPackage(), GenericPackage.Child, SpecificPackage.RootInstance, GenericPackage.Root_child, IStatementPosition.AT_END);
+		assertEquals(6, Select.objectsForRelation(fSession, GenericPackage.Root_child, SpecificPackage.RootInstance).size());
 	}
 }
